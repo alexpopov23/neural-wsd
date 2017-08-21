f9eeb7eed42fa2e9563b6de4af226fda2623e420
Alexander Popov <alexander.popov@ontotext.com>
2017-08-21 16:49:42 +0300

    deleted unnecessary files

--- apply_model.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-
-with tf.Session(graph=tf.Graph()) as session:
-    saver = tf.train.Saver()
-    saver.restore(session, ос.path.joint("save-path", "model.ckpt-25000.data-00000-of-00001"))

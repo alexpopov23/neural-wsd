f1b81d72d0213e3fe1d14bed1c1cf51902eeb4d4
Alexander Popov <alexander.popov@ontotext.com>
2017-10-26 15:16:42 +0300

    test

--- data_ops.py
+++ data_ops.py
@@ -366,6 +366,7 @@ def format_data (wsd_method, input_data, src2id, src2id_lemmas, synset2id, seq_w
                     else:
                         current_input_lemmas.append(src2id_lemmas["UNK"])
             elif word_embedding_input == "lemma":
+                # test
                 if word[1] in src2id:
                     current_input.append(src2id[word[1]])
                 else:

1c3b7095f2978626f7c1633a0468b2a0f44c9773
Alexander Popov <alexander.popov@ontotext.com>
2018-10-05 16:34:43 +0300

    added code for self-attention

--- architectures/abstract_model.py
+++ architectures/abstract_model.py
@@ -159,6 +159,14 @@ class AbstractModel:
             rnn_outputs, _ = tf.nn.bidirectional_dynamic_rnn(fw_multicell, bw_multicell, embedded_inputs,
                                                              dtype="float32", sequence_length=seq_lengths)
             rnn_outputs = tf.concat(rnn_outputs, 2)
+            rnn_outputs = self.multi_head_attention(rnn_outputs, rnn_outputs, 8, None, drop_rate=(1-self.keep_prob),
+                                                    is_train=is_training)
+            # attention_mechanism = tf.contrib.seq2seq.LuongAttention(
+            #     n_hidden * 2, attention_states,
+            #     memory_sequence_length=seq_lengths)
+            # attention_cell = tf.contrib.seq2seq.AttentionWrapper(
+            #     decoder_cell, attention_mechanism,
+            #     attention_layer_size=num_units)
             scope.reuse_variables()
             rnn_outputs = tf.reshape(rnn_outputs, [-1, 2 * n_hidden])
             logits_pos, losses_pos, cost_pos = [], [], 0.0
@@ -174,4 +182,58 @@ class AbstractModel:
     def output_layer(self, rnn_outputs, is_training, classif_type):
         """Calculates the output of the network, specific to the concrete types of models"""
         raise Exception ("Not implemented!")
+
+    def layer_normalize(self, inputs, epsilon=1e-8, scope=None):
+        with tf.variable_scope(scope or "layer_norm"):
+            inputs_shape = inputs.get_shape()
+            params_shape = inputs_shape[-1:]
+            mean, variance = tf.nn.moments(inputs, [-1], keep_dims=True)
+            beta = tf.Variable(tf.zeros(params_shape))
+            gamma = tf.Variable(tf.ones(params_shape))
+            normalized = (inputs - mean) / ((variance + epsilon) ** 0.5)
+            outputs = tf.add(tf.multiply(gamma, normalized), beta)
+            return outputs
+
+    def multi_head_attention(self, queries, keys, num_heads, attention_size, drop_rate=0.0, is_train=True, reuse=None,
+                             scope=None):
+        # borrowed from: https://github.com/Kyubyong/transformer/blob/master/modules.py
+        with tf.variable_scope(scope or "multi_head_attention", reuse=reuse):
+            if attention_size is None:
+                attention_size = queries.get_shape().as_list()[-1]
+            # linear projections, shape=(batch_size, max_time, attention_size)
+            query = tf.layers.dense(queries, attention_size, activation=tf.nn.relu, name="query_project")
+            key = tf.layers.dense(keys, attention_size, activation=tf.nn.relu, name="key_project")
+            value = tf.layers.dense(keys, attention_size, activation=tf.nn.relu, name="value_project")
+            # split and concatenation, shape=(batch_size * num_heads, max_time, attention_size / num_heads)
+            query_ = tf.concat(tf.split(query, num_heads, axis=2), axis=0)
+            key_ = tf.concat(tf.split(key, num_heads, axis=2), axis=0)
+            value_ = tf.concat(tf.split(value, num_heads, axis=2), axis=0)
+            # multiplication
+            attn_outs = tf.matmul(query_, tf.transpose(key_, [0, 2, 1]))
+            # scale
+            attn_outs = attn_outs / (key_.get_shape().as_list()[-1] ** 0.5)
+            # key masking
+            key_masks = tf.sign(tf.abs(tf.reduce_sum(keys, axis=-1)))  # shape=(batch_size, max_time)
+            key_masks = tf.tile(key_masks, [num_heads, 1])  # shape=(batch_size * num_heads, max_time)
+            # shape=(batch_size * num_heads, max_time, max_time)
+            key_masks = tf.tile(tf.expand_dims(key_masks, 1), [1, tf.shape(queries)[1], 1])
+            paddings = tf.ones_like(attn_outs) * (-2 ** 32 + 1)
+            # shape=(batch_size, max_time, attention_size)
+            attn_outs = tf.where(tf.equal(key_masks, 0), paddings, attn_outs)
+            # activation
+            attn_outs = tf.nn.softmax(attn_outs)
+            # query masking
+            query_masks = tf.sign(tf.abs(tf.reduce_sum(queries, axis=-1)))
+            query_masks = tf.tile(query_masks, [num_heads, 1])
+            query_masks = tf.tile(tf.expand_dims(query_masks, -1), [1, 1, tf.shape(keys)[1]])
+            attn_outs *= query_masks
+            # dropout
+            attn_outs = tf.layers.dropout(attn_outs, rate=drop_rate, training=is_train)
+            # weighted sum
+            outputs = tf.matmul(attn_outs, value_)
+            # restore shape
+            outputs = tf.concat(tf.split(outputs, num_heads, axis=0), axis=2)
+            outputs += queries  # residual connection
+            outputs = self.layer_normalize(outputs)
+            return outputs
 
